5db9c85658e362b98532eb6cd5836d59167e8cff
invincibleo <tangduowei@live.com>
2017-08-22 10:11:37 +0200

    mfcc

--- application/LearnerInceptionV3.py
+++ application/LearnerInceptionV3.py
@@ -357,7 +357,9 @@ def InceptionV3(include_top=True,
         # Classification block
         x = GlobalAveragePooling2D(name='avg_pool')(x)
         # x = Dropout(rate=0.8)(x)   ####### added by me
+        x = Dense(512, activation='relu', name='2ndLastPrediction')(x)   #####change softmax to sigmoid
         x = Dense(classes, activation='softmax', name='predictions')(x)   #####change softmax to sigmoid
+
     else:
         if pooling == 'avg':
             x = GlobalAveragePooling2D()(x)
